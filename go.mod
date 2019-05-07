79999e4f48076c1fc78c922ec50d9c1a2590eccb
int3h <int3h@cube.cube.biz>
2019-05-07 23:25:02 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/int3hh/burscrap
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.0 // indirect
+	github.com/akamensky/argparse v0.0.0-20190309155458-28b0496b54cb
+	github.com/antchfx/htmlquery v1.0.0 // indirect
+	github.com/antchfx/xmlquery v1.0.0 // indirect
+	github.com/antchfx/xpath v0.0.0-20190319080838-ce1d48779e67 // indirect
+	github.com/clagraff/argparse v1.0.1 // indirect
+	github.com/common-nighthawk/go-figure v0.0.0-20180619031829-18b2b544842c
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/logrusorgru/aurora v0.0.0-20190428105938-cea283e61946
+	github.com/mattn/go-runewidth v0.0.4 // indirect
+	github.com/nsf/termbox-go v0.0.0-20190325093121-288510b9734e // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/temoto/robotstxt v0.0.0-20180810133444-97ee4a9ee6ea // indirect
+	golang.org/x/net v0.0.0-20190503192946-f4e77d36d62c // indirect
+	google.golang.org/appengine v1.5.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,77 @@
+package main // import "github.com/int3hh/burscrap"
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"os"
+	"path"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/akamensky/argparse"
+	"github.com/common-nighthawk/go-figure"
+	"github.com/gocolly/colly"
+	. "github.com/logrusorgru/aurora"
+)
+
+var date *string
+var dirpath string
+
+func init() {
+	figure := figure.NewFigure("BurScrap", "", true)
+	figure.Print()
+	parser := argparse.NewParser("BurScrap", "Scraps Bursa news paper for articles")
+	date = parser.String("d", "date", &argparse.Options{Required: true, Help: "Scrap paper from day"})
+	err := parser.Parse(os.Args)
+	if err != nil {
+		log.Panic("Invalid arguments")
+	}
+	_, err = time.Parse("2006-01-02", *date)
+	if err != nil {
+		log.Panic("Bad date specified")
+	}
+
+	dirpath = path.Join("extracted", *date)
+	os.MkdirAll(dirpath, os.ModePerm)
+
+}
+
+func isNumeric(val string) bool {
+	_, err := strconv.ParseFloat(val, 32)
+	return err == nil
+}
+
+func main() {
+	fmt.Printf("Collecting data from %s \n", Green("http://www.bursa.ro/ziar/"+*date))
+	collect := colly.NewCollector(colly.AllowedDomains("www.bursa.ro"))
+	collect.OnRequest(func(r *colly.Request) {
+		fmt.Printf("Browsing to %s \n", Yellow(r.URL))
+	})
+	collect.OnHTML("header.caseta-medie-header a[href]", func(e *colly.HTMLElement) {
+		if strings.Contains(e.Request.URL.String(), "ziar/") {
+			link := e.Attr("href")
+			links := strings.Split(link, "-")
+			if len(links) > 0 && isNumeric(links[len(links)-1]) {
+				collect.Visit(e.Request.AbsoluteURL(link))
+			}
+		}
+	})
+	collect.OnHTML("#articol-text", func(e *colly.HTMLElement) {
+		title := e.Request.URL.RequestURI()
+		title = title[1:strings.LastIndex(title, "-")] + ".txt"
+		fil, err := os.Create(path.Join(dirpath, title))
+		if err == nil {
+			defer fil.Close()
+			fmt.Printf("Writing article : %s \n", Magenta(title))
+			e.ForEach("p.par", func(_ int, elem *colly.HTMLElement) {
+				text := html.UnescapeString(elem.Text) + "\n"
+				fil.Write([]byte(text))
+			})
+		} else {
+			fmt.Printf("Could not write file %s : %s \n", Yellow(title), Red(err.Error()))
+		}
+	})
+	collect.Visit("http://www.bursa.ro/ziar/" + *date)
+}
